3b609c759393a9e3c2d9cc697c62d23b197b568c
Tarun Verghis <tarun.verghis@gmail.com>
2024-09-30 23:14:35 -0700

    Section 03 Exercise 02

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -1,3 +1,5 @@
+const ALLOWED_STATUSES: [&'static str; 3] = ["To-Do", "In Progress", "Done"];
+
 struct Ticket {
     title: String,
     description: String,
@@ -17,7 +19,26 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+
+        if title.bytes().len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+
+        if description.bytes().len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+
+        if !ALLOWED_STATUSES.contains(&status.as_str()) {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed.");
+        }
+
         Self {
             title,
             description,
