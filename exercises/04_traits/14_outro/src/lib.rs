f6148b55ec6e8b5bec2ff4bab076bd22292876a0
Tarun Verghis <tarun.verghis@gmail.com>
2024-10-06 10:20:56 -0700

    Section 04 Exercise 14

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,73 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+macro_rules! impl_from_num {
+    ($($ntype:ty),*) => {
+        $(
+            impl From<$ntype> for SaturatingU16 {
+                fn from(v: $ntype) -> SaturatingU16 {
+                    SaturatingU16 { value: v.into() }
+                }
+            }
+            impl<'a> From<&'a $ntype> for SaturatingU16 {
+                fn from(v: &'a $ntype) -> SaturatingU16 {
+                    SaturatingU16 { value: (*v).into() }
+                }
+            }
+        )*
+    };
+}
+
+impl_from_num!(u8, u16);
+
+impl std::ops::Add<SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: SaturatingU16) -> Self {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs.value),
+        }
+    }
+}
+
+impl std::ops::Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &SaturatingU16) -> Self {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs.value),
+        }
+    }
+}
+
+impl std::ops::Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: u16) -> Self {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs),
+        }
+    }
+}
+
+impl std::ops::Add<&u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &u16) -> Self {
+        SaturatingU16 {
+            value: self.value.saturating_add(*rhs),
+        }
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, rhs: &u16) -> bool {
+        self.value == *rhs
+    }
+}
