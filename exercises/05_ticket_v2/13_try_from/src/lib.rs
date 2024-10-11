1ce39fddf9d375eed919976dea2271f0616b3ce4
Tarun Verghis <tarun.verghis@gmail.com>
2024-10-10 22:10:32 -0700

    Section 05 Exercise 13

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -8,6 +8,27 @@ enum Status {
     Done,
 }
 
+impl TryFrom<String> for Status {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Self::try_from(value.as_str())
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Self::ToDo),
+            "inprogress" => Ok(Self::InProgress),
+            "done" => Ok(Self::Done),
+            _ => Err("Failed to convert from String to Status".into())
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
