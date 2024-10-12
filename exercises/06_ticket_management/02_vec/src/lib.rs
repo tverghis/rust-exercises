0aa1d128c9deda53705261f33ac7c77783f3a0a3
Tarun Verghis <tarun.verghis@gmail.com>
2024-10-11 18:16:52 -0700

    Section 06 Exercise 02

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,13 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut fibs: Vec<u32> = vec![0, 1];
+
+    for i in 2..=(n as usize) {
+        fibs.push(fibs[i - 1] + fibs[i - 2]);
+    }
+
+    fibs[n as usize]
 }
 
 #[cfg(test)]
