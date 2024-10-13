c8eab83ec0638bd481c45a82fb74682989758c0f
Tarun Verghis <tarun.verghis@gmail.com>
2024-10-12 19:05:28 -0700

    Section 06 Exercise 11

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,12 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+pub fn squared(items: &mut [i32]) {
+    for i in 0..items.len() {
+        items[i] = items[i].pow(2)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
