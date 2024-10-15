e42ec8b2d5a7fe0e719fb7b27e6dd10e8572eb4f
Tarun Verghis <tarun.verghis@gmail.com>
2024-10-15 00:55:43 -0700

    Section 08 Exercise 05

--- exercises/08_futures/05_blocking/src/lib.rs
+++ exercises/08_futures/05_blocking/src/lib.rs
@@ -9,10 +9,12 @@ pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
     loop {
         let (socket, _) = listener.accept().await?;
         let mut socket = socket.into_std()?;
-        socket.set_nonblocking(false)?;
-        let mut buffer = Vec::new();
-        socket.read_to_end(&mut buffer)?;
-        socket.write_all(&buffer)?;
+        socket.set_nonblocking(false).unwrap();
+        tokio::task::spawn_blocking(move || {
+            let mut buffer = Vec::new();
+            socket.read_to_end(&mut buffer).unwrap();
+            socket.write_all(&buffer).unwrap();
+        });
     }
 }
 
